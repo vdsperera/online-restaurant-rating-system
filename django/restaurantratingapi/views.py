b80c9d686f0d577fac5c6746d51bca4480ba1240
vidumini <heartyvds@gmail.com>
2020-07-30 12:49:56 +0530

    created view set for restaurant registration

--- django/restaurantratingapi/views.py
+++ django/restaurantratingapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .services.RestaurantService import RestaurantService
 
-# Create your views here.
+class RestaurantViewSet(viewsets.ViewSet):
+	
+	def create(self, request):
+		data = request.data['data']['mdata']
+		restservice = RestaurantService()
+		response = restservice.register_restaurant(data)
+		return Response(response)
