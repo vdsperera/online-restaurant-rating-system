2de77061b0066e6d7c5275c0d3d846c6d0e6b2e7
vidumini <heartyvds@gmail.com>
2020-07-30 12:21:21 +0530

    added methods for restaurant service

---
+++ django/restaurantratingapi/services/RestaurantService.py
@@ -0,0 +1,29 @@
+
+class RestaurantService:
+
+    def __init__(self):
+        self.data = []
+
+    def register_restaurant(self, data):
+        pass
+        
+    def deregister_restaurant():
+        pass    
+        
+    def get_restaurant():
+        pass
+
+    def get_restaurant_list():
+        pass
+
+    def request_edit():
+        pass
+
+    def approve_edit():
+        pass
+
+    def claim_restaurant():
+        pass
+
+
+
