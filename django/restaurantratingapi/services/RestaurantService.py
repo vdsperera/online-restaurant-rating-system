2fa7840a92c23b642b78511e36dab466dd6a8bfb
vidumini <heartyvds@gmail.com>
2020-07-30 12:37:10 +0530

    set restaurant registration method

--- django/restaurantratingapi/services/RestaurantService.py
+++ django/restaurantratingapi/services/RestaurantService.py
@@ -1,3 +1,9 @@
+from .ValidationService import ValidationService
+from .SystemService import SystemService
+from ..enums.RestaurantEnums import ClaimStatus
+from ..models import Restaurant
+from django.contrib.auth.models import User
+from rest_framework.exceptions import APIException
 
 class RestaurantService:
 
@@ -5,7 +11,89 @@ class RestaurantService:
         self.data = []
 
     def register_restaurant(self, data):
-        pass
+        # retrieve request data
+        username = data['user']
+        rest_name = data['name'] #restaurant name #required
+        rest_address = data['address'] #restaurant address
+        rest_pnumber = data['phone_number'] #restaurant phone number
+        rest_longitude = data['longitude'] #restaurant GEO location-longitude #required
+        rest_latitude = data['latitude'] #restaurant GEO location-latitude #required
+        selected_role = data['role'] #user's requested role for the restaurant
+
+        # set default values
+        rest_code = SystemService.generate_restaurant_code()
+        rest_claimed = ClaimStatus.Unclaimed.value
+        rest_owner = None
+
+        # validate request data for null or empty values
+        if(not ValidationService.isset(value=rest_name)):
+            raise APIException("Restaurant name is empty")
+
+        if(not ValidationService.isset(value=username)):
+            raise APIException("User is empty")
+
+        if(not ValidationService.isset(value=rest_longitude)):
+            raise APIException("Longitude is empty")    
+
+        if(not ValidationService.isset(value=rest_latitude)):
+            raise APIException("Latitude is empty")    
+
+        # check whether the user is owner - raw sql should be replaced
+        users = User.objects.raw('SELECT * FROM auth_user INNER JOIN auth_user_groups ON auth_user.id=auth_user_groups.user_id WHERE auth_user_groups.group_id=1 AND auth_user.username=%s', [username])
+        
+        # retrieve user object for signed in user
+        user = User.objects.get(username=username)
+
+        if(not users):
+            print("User is not an owner")
+            if(selected_role=='owner'):
+                pass
+                ###Redirect to customer-owner transformation process and end current process###
+        else:
+            print("User is an owner")
+            rest_owner = user
+            rest_claimed = ClaimStatus.Pending.value
+            ### Need to send generated code ###
+
+        # create restaurant - need to handle exceptions
+        rest = Restaurant(
+            name=rest_name,
+            address=rest_address,
+            longitude=rest_longitude,
+            latitude=rest_latitude,            
+            phone_number=rest_pnumber,
+            claimed=rest_claimed,
+            claimed_by=rest_owner,
+            code=rest_code,
+            created_by=user)      
+         
+        rest.save()
+        
+        print(rest_owner.id if rest_owner != None else None)
+
+        ### Need to add contribution points to the user ###
+
+        # this should be change by using response model
+        respose={
+            "success": True,
+            "code": 200,
+            "message": "success RegisterRestaurant",
+            "data": {
+                "restaurant_id": rest.restaurant_id,
+                "restaurant_name": rest.name,
+                "address": rest.address,
+                "logitude": rest.longitude,
+                "latitude": rest.latitude,        
+                "phone_number": rest.phone_number,
+                "added_by": rest.created_by.id,
+                "claimed_by": rest_owner.id if rest_owner != None else None,
+                "code": rest.code,
+                "claimed_status": ClaimStatus(rest.claimed).name,
+                "created_on": rest.created_on
+            }
+        }
+
+        return response  	
         
     def deregister_restaurant():
         pass    
