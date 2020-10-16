ce96725c4f4b2772c487c3c9a5c210b27e0335b5
vidumini <heartyvds@gmail.com>
2020-10-16 19:44:58 +0530

    set components in app module

--- angular/restaurant-rating-system/src/app/app.module.ts
+++ angular/restaurant-rating-system/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
+import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantListComponent,
+    ToolbarComponent,
+    RegisterRestaurantComponent,
+    RestaurantDetailsComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
