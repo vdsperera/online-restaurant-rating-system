6f061fc2254ff32f5c157f4dffbf3d19addcf87c
vidumini <heartyvds@gmail.com>
2020-10-16 20:43:06 +0530

    added logic for restaurant list

--- angular/restaurant-rating-system/src/app/restaurant-list/restaurant-list.component.ts
+++ angular/restaurant-rating-system/src/app/restaurant-list/restaurant-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs'
+import { Router } from '@angular/router'
+import { ApiService } from '../api.service' 
+import { Restaurant } from '../restaurant';
 
 @Component({
   selector: 'app-restaurant-list',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantListComponent implements OnInit {
 
-  constructor() { }
+  restaurants$: Observable<Restaurant[]>;
+
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
+    this.get_restaurants();
+    console.log(this.restaurants$);
+  }
+
+  public get_restaurants() {
+    this.restaurants$ = this.apiService.get_restaurants();
+
+  }
+
+  public view_details(restaurant_id) {
+    console.log(restaurant_id)
+    this.router.navigate(['/restaurants', restaurant_id]);
   }
 
 }
