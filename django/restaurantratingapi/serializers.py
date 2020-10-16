ac466aa0a07c4d31dc5a40e5837b4fdf40b93b52
vidumini <heartyvds@gmail.com>
2020-10-16 16:11:34 +0530

    added restaurant serializer

--- django/restaurantratingapi/serializers.py
+++ django/restaurantratingapi/serializers.py
@@ -9,3 +9,8 @@ class UserRoleSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = UserRole
 		fields = ['role_id', 'role_name']
+
+class RestaurantSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ['restaurant_id', 'name', 'address', 'longitude','latitude', 'phone_number', 'code','claimed', 'claimed_by', 'created_by']
