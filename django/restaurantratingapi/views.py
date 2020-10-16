c4139ea2039074fb15e025195cab98a6213a5051
vidumini <heartyvds@gmail.com>
2020-10-16 16:18:45 +0530

    set view set for user role

--- django/restaurantratingapi/views.py
+++ django/restaurantratingapi/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .services.RestaurantService import RestaurantService
 
+class UserRoleViewSet(viewsets.ModelViewSet):
+	queryset = UserRole.objects.all().order_by('role_name')
+	serializer_class = UserRoleSerializer
+
 class RestaurantViewSet(viewsets.ViewSet):
 	
 	def create(self, request):
