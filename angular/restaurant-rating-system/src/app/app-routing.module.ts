242838c252aae7245ffbfb46aa31bde08e8d475b
vidumini <heartyvds@gmail.com>
2020-10-16 20:41:19 +0530

    added angular app routing module

--- angular/restaurant-rating-system/src/app/app-routing.module.ts
+++ angular/restaurant-rating-system/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RestaurantListComponent},
+  {path: 'registerrestaurant', component: RegisterRestaurantComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
