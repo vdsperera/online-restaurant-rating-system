798027dba021f7f1203400ed4511c0ec7e1e04b4
vidumini <heartyvds@gmail.com>
2020-10-17 13:16:59 +0530

    added logic for toolbar component

--- angular/restaurant-rating-system/src/app/toolbar/toolbar.component.ts
+++ angular/restaurant-rating-system/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,5 +12,59 @@ export class ToolbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Blog',
+      icon: 'rss_feed',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: false,
+      route: ''
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true,
+      route: ''
+    },
+    {
+      label: 'Docs',
+      icon: 'notes',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true,
+      route: ''
+    },
+    {
+      label: 'Add Restaurant',
+      icon: 'add_circle',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true,
+      route: '/registerrestaurant'
+    },
+    {
+      label: 'About',
+      icon: 'help',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true,
+      route: ''
+    },
+    {
+      label: 'Sign Up',
+      icon: 'login',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true,
+      route: ''
+    },
+
+  ];
+
+
 
 }
