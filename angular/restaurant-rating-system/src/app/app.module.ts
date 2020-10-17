dc27f4b7db35c8ee35a9d7055da7cf6e8bc3ce05
vidumini <heartyvds@gmail.com>
2020-10-17 13:13:45 +0530

    updated app module

--- angular/restaurant-rating-system/src/app/app.module.ts
+++ angular/restaurant-rating-system/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
 import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 import { StarRatingComponent } from './star-rating/star-rating.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -23,8 +38,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    MatSidenavModule,    
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,   
+    MatSnackBarModule,   
+    FlexLayoutModule,      
   ],
   providers: [],
   bootstrap: [AppComponent]
