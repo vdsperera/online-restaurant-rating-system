4e135f6f40b7d48e86c84ff97572bf4dcd91c7e3
vidumini <heartyvds@gmail.com>
2020-10-17 16:48:51 +0530

    updated allowed hosts list

--- django/restaurantratingsystem/settings.py
+++ django/restaurantratingsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kl1_89rom85%#acsi__$v$2e5hf)f4sa**_gu#$94su^jxjk43'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
