f59d99f1161ea4a30c3447a1390c63925d8a6c7d
vidumini <heartyvds@gmail.com>
2020-10-17 13:16:17 +0530

    added logic for restaurant details component

--- angular/restaurant-rating-system/src/app/restaurant-details/restaurant-details.component.ts
+++ angular/restaurant-rating-system/src/app/restaurant-details/restaurant-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Rating } from '../models/rating';
 
 @Component({
   selector: 'app-restaurant-details',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailsComponent implements OnInit {
 
+  public snack_bar_duration: number = 2000;
+  public rating_array = [];
+  public rating_model = new Rating;
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  rating_event_hander($event: any)
+  {
+    this.rating_model = $event;
+    console.log('even emitter');
+    console.log(this.rating_model);
   }
 
 }
