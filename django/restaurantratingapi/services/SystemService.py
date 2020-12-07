20336ad0437e69142b7bcf92e8dfc3750aa372c0
vidumini <heartyvds@gmail.com>
2020-12-07 16:02:32 +0530

    added method for generating ids

--- django/restaurantratingapi/services/SystemService.py
+++ django/restaurantratingapi/services/SystemService.py
@@ -1,4 +1,15 @@
 from django.utils.crypto import get_random_string
+from .ValidationService import ValidationService
+from ..models import Token
+from ..models import Restaurant
+from ..models import Contribution
+from ..models import ContributionType
+from django.contrib.auth.models import User
+from rest_framework.exceptions import APIException
+from rest_framework.exceptions import NotFound
+from django.db import IntegrityError
+import string
+import random
 
 class SystemService:
 
@@ -8,3 +19,7 @@ class SystemService:
     @staticmethod
     def generate_restaurant_code():
     	return get_random_string(length=20)
+
+    @staticmethod
+    def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
