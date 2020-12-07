739633f1cb9be4a8b55f5bc6d4a07676d1119731
vidumini <heartyvds@gmail.com>
2020-12-07 16:04:08 +0530

    added method for generating tokens for restaurant

--- django/restaurantratingapi/services/SystemService.py
+++ django/restaurantratingapi/services/SystemService.py
@@ -23,3 +23,58 @@ class SystemService:
     @staticmethod
     def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
+
+    def generate_tokens_for_restaurant(self, data):
+    	
+        restaurant_id = data['restaurant_id']
+        username = data['user']
+        generate_size = data['generate_size']
+
+        try: 
+            restaurant = Restaurant.objects.get(restaurant_id=restaurant_id)
+        except IntegrityError as e:
+            raise APIException("Restaurant is not available in the system")
+
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist as e:
+            raise APIException(f"Username name '{username}' not exists")
+
+        i=0
+        token_number_list = []
+        while i < generate_size:
+            i = i + 1
+            token_number = SystemService.id_generator() 
+            check_token = Token.objects.filter(token_number=token_number)
+
+            if(check_token.exists()):
+                i = i - 1
+                continue
+            try:
+                token = Token(
+                    token_number = token_number,
+                    restaurant = restaurant,
+                    created_on = '2020-11-15',
+                    created_by = user
+                    )
+                token.save()
+            except IntegrityError as e:
+                raise APIException("Adding token is failed")
+            token_number_model = {
+              "token_number": token_number
+            }
+            token_number_list.append(token_number_model)
+
+        resp = {
+        "success": True,
+        "code": 200,
+        "message": "success GenerateTokesForRestaurant",
+        "data": {
+            "restaurant_id": restaurant_id,
+            "generate_size": generate_size,
+            "added_by": user.username,
+            "tokens": token_number_list,
+        }
+        }
+
+        return resp   
