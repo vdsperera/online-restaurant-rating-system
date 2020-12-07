841b7be47e61aa2be7bb8997039f17a7492a85f3
vidumini <heartyvds@gmail.com>
2020-12-07 15:51:55 +0530

    set modification in register restaurant
    *divide address
    *add contribution points

--- django/restaurantratingapi/services/RestaurantService.py
+++ django/restaurantratingapi/services/RestaurantService.py
@@ -1,9 +1,26 @@
 from .ValidationService import ValidationService
 from .SystemService import SystemService
-from ..enums.RestaurantEnums import ClaimStatus
+from ..enums.RestaurantEnums import ClaimStatus, RestaurantComponents
+from ..enums.ContributionEnums import ContributionTypes
+from ..enums.UserEnums import UserRoles
 from ..models import Restaurant
 from django.contrib.auth.models import User
+from ..models import CustomUser
+from..models import EditHistory
+from..models import EditComponent
+from..models import UserEditHistoryComponent
+from..models import UserEditHistoryConfirmation
 from rest_framework.exceptions import APIException
+from rest_framework.exceptions import NotFound
+from django.db import IntegrityError
+from django.core.validators import RegexValidator
+import re
+from .RatingService import RatingService
+from ..models import Rating
+from ..models import Dish
+from ..models import RestaurantDish
+from django.core.exceptions import ObjectDoesNotExist
+import string
 
 class RestaurantService:
 
@@ -16,18 +33,25 @@ class RestaurantService:
         try:
             username = data['user']
             rest_name = data['name'] #restaurant name #required
-            rest_address = data['address'] #restaurant address
+            # rest_address = data['address'] #restaurant address
+            rest_street = data['address']['street_address'] + ', '
+            rest_city = string.capwords(data['address']['city'])
+            print(rest_street+ rest_city)
             rest_pnumber = data['phone_number'] #restaurant phone number
+            # rest_added_by = data['user'] #restaurant is added by #required
+            # rest_owner = data['owner'] #owner of the restaurant
             rest_longitude = data['longitude'] #restaurant GEO location-longitude #required
             rest_latitude = data['latitude'] #restaurant GEO location-latitude #required
             selected_role = data['role'] #user's requested role for the restaurant
         except KeyError as e:
+            print(f"Key {e} not exists in the request")
             raise APIException(f"Key {e} not exists in the request")
 
         # set default values
         rest_code = SystemService.generate_restaurant_code()
         rest_claimed = ClaimStatus.Unclaimed.value
         rest_owner = None
+        rest_address = data['address']['street_address'] + ', ' + data['address']['city']
 
         # validate request data for null or empty values
         if(not ValidationService.isset(value=rest_name)):
@@ -36,13 +60,19 @@ class RestaurantService:
         if(not ValidationService.isset(value=username)):
             raise APIException("User is empty")
 
+        if(not ValidationService.isset(value=rest_city)):
+            raise APIException("City is empty")
+
         if(not ValidationService.isset(value=rest_longitude)):
             raise APIException("Longitude is empty")    
 
         if(not ValidationService.isset(value=rest_latitude)):
             raise APIException("Latitude is empty")    
 
+
+
         # validate request data values
+
         if(not isinstance(username, str)):
             raise APIException("Username should be string")
 
@@ -73,8 +103,12 @@ class RestaurantService:
         if(not ValidationService.is_geo_coordinate(rest_latitude,'la')):
             raise APIException("Latitude is invalid")
 
+        
+        # mobile_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
         if(not ValidationService.is_phone_number(rest_pnumber)):
             raise APIException("Phone number is invalid")
+
+        # user = User.objects.get(username='vidumini')   
 
         #check whether the restaurant name already exists
         if(Restaurant.objects.filter(name=rest_name).exists()):
@@ -83,16 +117,17 @@ class RestaurantService:
         #check whether the username already exists
         if(not User.objects.filter(username=username).exists()):
             raise APIException(f"Username name '{username}' not exists")
-
-        # retrieve user object for signed in user
-        user = User.objects.get(username=username)
+        
+        user = User.objects.get(username=username)    
 
         # check whether the user is owner - raw sql should be replaced
         users = User.objects.raw('SELECT * FROM auth_user INNER JOIN auth_user_groups ON auth_user.id=auth_user_groups.user_id WHERE auth_user_groups.group_id=1 AND auth_user.username=%s', [username])
 
+        # print(type(users))
         if(not users):
             print("User is not an owner")
             if(selected_role=='owner'):
+                raise APIException(f"User '{username}' currently not a owner")
                 pass
                 ###Redirect to customer-owner transformation process and end current process###
         else:
@@ -100,6 +135,9 @@ class RestaurantService:
             rest_owner = user
             rest_claimed = ClaimStatus.Pending.value
             ### Need to send generated code ###
+
+        # for u in users:
+        #     print(u)
 
         # create restaurant - need to handle exceptions
         rest = Restaurant(
@@ -113,19 +151,28 @@ class RestaurantService:
             code=rest_code,
             created_by=user)      
          
-        rest.save()
+        try: 
+            rest.save()
+        except IntegrityError as e:
+            raise APIException(e)
         
         print(rest_owner.id if rest_owner != None else None)
+        # print(type(rest))
 
         ### Need to add contribution points to the user ###
+        # print(ContributionTypes.AddVerifiedDishRating.name)
+        if(not users):
+            system_service = SystemService()
+            system_service.add_contribution_points(ContributionTypes.AddRestaurant.value, user)
+
 
         # this should be change by using response model
-        response={
+        resp={
             "success": True,
             "code": 200,
             "message": "success RegisterRestaurant",
             "data": {
-                "restaurant_id": rest.restaurant_id,
+                # "restaurant_id": rest.restaurant_id,
                 "restaurant_name": rest.name,
                 "address": rest.address,
                 "logitude": rest.longitude,
@@ -139,7 +186,7 @@ class RestaurantService:
             }
         }
 
-        return response  	
+        return resp   	
         
     def deregister_restaurant():
         pass    
