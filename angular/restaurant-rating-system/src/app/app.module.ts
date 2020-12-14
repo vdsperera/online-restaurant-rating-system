f290d1ef258137749bb3fde23ccd6480aebbd9a9
vidumini <heartyvds@gmail.com>
2020-12-14 15:02:35 +0530

    added imports and declarations in app module

--- angular/restaurant-rating-system/src/app/app.module.ts
+++ angular/restaurant-rating-system/src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
-import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
-import { StarRatingComponent } from './star-rating/star-rating.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
+// import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
+import { StarRatingComponent } from './star-rating/star-rating.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { IntercepterService } from './services/intercepter.service';
+import { ModalModule } from './_modal'
 
 @NgModule({
   declarations: [
@@ -33,29 +38,44 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     ToolbarComponent,
     RegisterRestaurantComponent,
     RestaurantDetailsComponent,
-    StarRatingComponent
+    StarRatingComponent,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    LayoutModule,
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatSidenavModule,
     MatSliderModule,
     MatCardModule,
+    FlexLayoutModule,
     MatButtonModule,
+    MatSidenavModule,
     MatToolbarModule,
+    // NavbarModule,
+    // WavesModule,
+    // ButtonsModule,
     MatIconModule,
     MatMenuModule,
+    // MDBBootstrapModule.forRoot(),
+    // MDBBootstrapModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule,   
-    MatSnackBarModule,   
-    FlexLayoutModule,      
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    ModalModule,
+
   ],
-  providers: [],
+  providers: [
+  {
+      provide: HTTP_INTERCEPTORS,
+      useClass: IntercepterService,
+      multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
